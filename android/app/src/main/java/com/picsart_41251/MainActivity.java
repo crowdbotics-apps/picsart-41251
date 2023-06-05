d8067be7e674f662f279bb3181e6b5e1305b3c32
Crowdbotics <team@crowdbotics.com>
2023-06-05 12:33:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/picsart_41251/MainActivity.java
@@ -0,0 +1,15 @@
+package com.picsart_41251;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "picsart_41251";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "picsart-41251",
+  url: "https://picsart-41251.botics.co",
+  api: "https://picsart-41251.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
